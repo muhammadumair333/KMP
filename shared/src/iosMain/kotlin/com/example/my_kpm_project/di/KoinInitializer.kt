ca5437edbabb8a0b299a658781f5d2cb09b3477e
Muhammad Umair <muhammad.umair@jeeny.me>
2025-11-06 12:30:14 +0500

    Koin initializer done with iosApp not working

---
+++ shared/src/iosMain/kotlin/com/example/my_kpm_project/di/KoinInitializer.kt
@@ -0,0 +1,18 @@
+package com.example.my_kpm_project.di
+
+import com.example.my_kpm_project.articles.ArticlesViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+fun initKoin() {
+    val modules = sharedKoinModules
+
+    startKoin {
+        modules(modules)
+    }
+}
+
+class ArticlesInjector : KoinComponent {
+    val articleViewModel: ArticlesViewModel by inject()
+}
